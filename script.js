7e16360085379a525515fb8191400da990785414
tezukamisaki <167516877+tezukamisaki@users.noreply.github.com>
2026-05-15 18:19:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+    data(){
+        return{
+            name:"手塚未祥",
+            course:"ITエンジニア",
+            food: "オムライス",
+            hoddy:"ゲーム.読書",
+            message:"よろしくお願いいたします。",
+            year:"3"
+
+
+        };
+    }
+}).mount("#app");
